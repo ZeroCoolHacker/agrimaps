5db2b608aa1979ed1adb1cb8daa0dbde92600aa7
oriefy <shahzaibakash@gmail.com>
2019-07-09 14:15:47 +0500

    add extra fields to faculty and department add them to admin

--- agri/admin.py
+++ agri/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Faculty, Department
+
 
 # Register your models here.
+
+admin.site.register(Faculty)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    '''Admin View for Department'''
+
+    list_display = (
+        'name',
+        'faculty',
+        'map_url'
+        )
+    list_filter = (
+        'faculty__name',
+        )
+
+    search_fields = (
+        'name',
+        )
+    ordering = ('faculty',)
--- agri/models.py
+++ agri/models.py
@@ -5,13 +5,14 @@ class Faculty(models.Model):
     """Model definition for Faculty."""
 
     name = models.CharField('Faculty Name', max_length=50)
+    dean = models.CharField('Dean', max_length=50)
 
 
     class Meta:
         """Meta definition for Faculty."""
 
         verbose_name = 'Faculty'
-        verbose_name_plural = 'Facultys'
+        verbose_name_plural = 'Faculties'
 
     def __str__(self):
         """Unicode representation of Faculty."""
@@ -20,8 +21,15 @@ class Faculty(models.Model):
 
 class Department(models.Model):
     """Model definition for Department."""
+    
+    
+    faculty     = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    name        = models.CharField('Department Name', max_length=50)
+    chairman    = models.CharField('Chairman', max_length=50)
+    description = models.TextField(default='Enter Description Here')
+    cover_photo = models.ImageField('Department Cover Photo', upload_to='departments/', null=True)
+    map_url     = models.URLField('Map URL', default='http://localhost')
 
-    name = models.CharField('Department Name', max_length=50)
 
     class Meta:
         """Meta definition for Department."""
