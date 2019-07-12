c56b6508458da882675c30133bda064bcc6ce71c
oriefy <shahzaibakash@gmail.com>
2019-07-12 23:37:11 +0500

    add admin and staff links add admin icon

--- agri_maps/urls.py
+++ agri_maps/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('department/<int:pk>', department, name='department'),
-    path('info', info, name='info')
+    path('staff', info, name='staff')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
